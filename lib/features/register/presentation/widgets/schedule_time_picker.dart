597bce73d0787d8ca68ecde93f9b449a9c652db8
dhimasdewanto <dhimasbrd@gmail.com>
2021-03-09 15:57:52 +0700

    Add ios time picker

--- lib/features/register/presentation/widgets/schedule_time_picker.dart
+++ lib/features/register/presentation/widgets/schedule_time_picker.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../controllers/register/register_controller.dart';
+import 'ios_date_time_container.dart';
 import 'schedule_select_button.dart';
 
 class ScheduleTimePicker extends StatelessWidget {
@@ -12,6 +16,14 @@ class ScheduleTimePicker extends StatelessWidget {
     final controller = inherit.controllerState;
     final schedule = inherit.scheduleVideoCall;
     final scheduleTime = schedule.time;
+    final now = DateTime.now();
+    final scheduleTimeDateTime = DateTime(
+      now.year,
+      now.month,
+      now.day,
+      scheduleTime?.hour ?? now.hour,
+      scheduleTime?.minute ?? now.minute,
+    );
 
     return ScheduleSelectButton(
       title: "Time",
@@ -19,10 +31,39 @@ class ScheduleTimePicker extends StatelessWidget {
           ? "- Choose Time -"
           : "${scheduleTime.hour.toString().padLeft(2, '0')}:${scheduleTime.minute.toString().padLeft(2, '0')}",
       onPressed: () async {
-        final newTime = await showTimePicker(
-          context: context,
-          initialTime: TimeOfDay.now(),
-        );
+        TimeOfDay? newTime;
+
+        if (Platform.isIOS) {
+          newTime = await showModalBottomSheet<TimeOfDay>(
+            context: context,
+            builder: (context) {
+              TimeOfDay? tempPickedTime;
+
+              return IOSDateTimeContainer(
+                onDone: () {
+                  Navigator.of(context).pop(tempPickedTime);
+                },
+                onCancel: () {
+                  Navigator.of(context).pop();
+                },
+                pickerWidget: CupertinoDatePicker(
+                  initialDateTime: scheduleTimeDateTime,
+                  mode: CupertinoDatePickerMode.time,
+                  use24hFormat: true,
+                  onDateTimeChanged: (dateTime) {
+                    tempPickedTime = TimeOfDay.fromDateTime(dateTime);
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          newTime = await showTimePicker(
+            context: context,
+            initialTime: scheduleTime ?? TimeOfDay.now(),
+          );
+        }
+
         controller.scheduleVideoCall = controller.scheduleVideoCall.copyWith(
           time: newTime,
         );
